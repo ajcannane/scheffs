58331fb61a6e252ceae470270e4045f0c8854a2d
Adam Cannane <ajcannane@gmail.com>
2026-07-10 23:31:06 +0930

    Convert inline contact form to a Bootstrap modal on all pages
    Contact Us now opens a modal (Bootstrap's JS plugin, styled to match
    the existing site design) on both index and gallery pages. Gallery
    previously had no contact form at all, only a link back to the index
    page's static address block. Successful submission now shows a
    dedicated success modal instead of an inline alert; the gallery page
    now also needs RECAPTCHA_SITE_KEY substituted into its template.

--- deployed_site/assets/js/contact.js
+++ deployed_site/assets/js/contact.js
@@ -12,14 +12,21 @@ $(function () {
                 data: $(this).serialize(),
                 success: function (data)
                 {
+                    if (data.type === 'success') {
+                        $('#contact-form').find('.messages').empty();
+                        $('#contact-form')[0].reset();
+                        grecaptcha.reset();
+                        $('#contactModal').modal('hide');
+                        $('#successModal').modal('show');
+                        return;
+                    }
+
                     var messageAlert = 'alert-' + data.type;
                     var messageText = data.message;
 
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><a href="#" class="close" data-dismiss="alert" aria-hidden="true">&times;</a>' + messageText + '</div>';
                     if (messageAlert && messageText) {
                         $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
-                        grecaptcha.reset();
                     }
                 }
             });
